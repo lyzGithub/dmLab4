70833e1a752e61df891a27e1e294a5b0afceca87
yize li <yize li>
2016-11-13 13:43:33 +0800

    hi

---
+++ dm3Main.m
@@ -0,0 +1,10 @@
+data1TestPath = 'data/data1/dataset1-a9a-testing.txt';
+data1TrainPath = 'data/data1/dataset1-a9a-training.txt';
+data2TestPath = 'data/data2/covtype-testing.txt';
+data2TrainPath = 'data/data2/covtype-training.txt';
+
+data1TestMatrix = load(data1TestPath);
+data1TrainMatrix = load(data1TrainPath);
+data2TestMatrix = load(data2TestPath);
+data2TrainMatrix = load(data2TrainPath);
+
